99195f75cbc0466f5142bf1ea210abe10e6bdfa8
Preston <henry.yu.becker@gmail.com>
2015-06-19 21:17:23 -0400

    Dashboard Enhancement

--- static/js/config.router.js
+++ static/js/config.router.js
@@ -33,7 +33,7 @@ angular.module('app')
       function ($stateProvider,   $urlRouterProvider) {
           
           $urlRouterProvider
-              .otherwise('/app/dashboard');
+              .otherwise('/app/dashboard/:pageId');
           $stateProvider
               .state('app', {
                   abstract: true,
@@ -41,8 +41,87 @@ angular.module('app')
                   templateUrl: '/static/templates/app.html'
               })
               .state('app.dashboard', {
-                  url: '/dashboard',
+                  url: '/dashboard/:pageId',
                   templateUrl: '/static/templates/app_dashboard.html',
+                  controller: ['$scope', '$stateParams', '$rootScope', '$http', '$state', '$facebook', '$location', '$cacheFactory', function($scope, $stateParams, $rootScope, $http, $state, $facebook, $location, $cacheFactory) {
+                    
+                    var today = new Date();
+                    var thirtydaysAgoTime = new Date().setDate(today.getDate()-30);
+                    var thirtydaysAgo = new Date(thirtydaysAgoTime);
+                    var dd = today.getDate();
+                    var mm = today.getMonth()+1; //January is 0!
+                    var yyyy = today.getFullYear();
+
+                    if(dd<10) {
+                        dd='0'+dd
+                    } 
+
+                    if(mm<10) {
+                        mm='0'+mm
+                    } 
+
+                    today = yyyy+'-'+mm+'-'+dd;
+
+                    var dd1 = thirtydaysAgo.getDate();
+                    var mm1 = thirtydaysAgo.getMonth()+1; //January is 0!
+                    var yyyy1 = thirtydaysAgo.getFullYear();
+
+                    if(dd1<10) {
+                        dd1='0'+dd1
+                    } 
+
+                    if(mm1<10) {
+                        mm1='0'+mm1
+                    } 
+
+                    thirtydaysAgo = yyyy1+'-'+mm1+'-'+dd1;
+
+                    $scope.pageId = $stateParams.pageId;
+
+                    $http({
+                        method: 'GET',
+                        url: '/dashboard/:pageId',
+                        cache: true
+                      });
+                    var cache = $cacheFactory.get('$http');
+                     console.log(cache);
+                     var pageCache = cache.get("/"+$scope.pageId+"?fields=id,feed,promotable_posts,name,category,link,likes,new_like_count,insights");
+                     
+                     console.log(pageCache);
+                     if(pageCache !== undefined) {
+                        $scope.pageData = pageCache;
+                     }
+
+                      $facebook.api("/"+$scope.pageId+"?fields=id,feed,promotable_posts,name,category,link,likes,new_like_count,insights").then(
+                        function(response) {
+                           $scope.pageData = response;
+                           console.log("/"+$scope.pageId+"/insights/page_impressions/day?since="+thirtydaysAgo+"&until="+today);
+                           $facebook.api("/"+$scope.pageId+"/insights/page_impressions/day?since="+thirtydaysAgo+"&until="+today).then(
+                            function(response) {
+
+                               
+                               var thirtydaysImpression =[];
+                               for(var i = 0; i < response.data[0].values.length; ++i) {
+                                  var dailyImp = [];
+                                  var str = response.data[0].values[i].end_time
+                                  var date = str.substring(5,10);
+                                  dailyImp.push(i+1);
+                                  dailyImp.push(response.data[0].values[i].value);
+                                  thirtydaysImpression.push(dailyImp);
+                               }
+                               
+                               //$scope.d0_1 = [ [0,7],[1,6.5],[2,12.5],[3,7],[4,9],[5,6],[6,11],[7,6.5],[8,8],[9,7] ];
+                               $scope.pageData.thirtydaysImpression = thirtydaysImpression;
+                               onsole.log($scope.pageData.thirtydaysImpression);
+                            },
+                            function(err) {
+                                  console.log("err");
+                            });   
+                        },
+                        function(err) {
+                              console.log("err");
+                        });                 
+                  }],
                   resolve: {
                     deps: ['$ocLazyLoad',
                       function( $ocLazyLoad ){
@@ -74,7 +153,7 @@ angular.module('app')
                       });
                      var cache = $cacheFactory.get('$http');
                      console.log(cache);
-                     var pageCache = cache.get("/"+$scope.pageId+"?fields=promotable_posts,id,name,category,link,likes,cover,username");
+                     var pageCache = cache.get("/"+$scope.pageId+"?fields=promotable_posts,feed,id,name,category,link,likes,cover,username");
                      
                      console.log(pageCache);
                      if(pageCache !== undefined) {
@@ -82,83 +161,75 @@ angular.module('app')
                         
                      }
 
-                      $facebook.api("/"+$scope.pageId+"?fields=promotable_posts,id,name,category,link,likes,cover,username").then(
+                      $facebook.api("/"+$scope.pageId+"?fields=promotable_posts,feed,id,name,category,link,likes,cover,username").then(
                         function(response) {
-                            console.log(response);
-                            cache.put("/"+$scope.pageId+"?fields=promotable_posts,id,name,category,link,likes,cover,username", response);
+                            //console.log(response);
+                            cache.put("/"+$scope.pageId+"?fields=promotable_posts,feed,id,name,category,link,likes,cover,username", response);
                             $scope.pageData = response;
                             $scope.pageData.cover.source = $scope.pageData.cover.source.replace(/\/[a-z][0-9]+x[0-9]+/, "");
                             var promotable_posts = $scope.pageData.promotable_posts.data;
-                            //var insights = [];
-                            //$scope.pageData.promotable_posts['insights'] = insights;
-                           
-                            $facebook.api("/"+$scope.pageId+"?fields=feed").then(
-                              function(response) {
-                                
-                                console.log(response);
-                                for(var i = 0; i < response.feed.data.length; i++) {
-                                    if(response.feed.data[i].status_type == "wall_post") {
-                                      $scope.pageData.promotable_posts.data.push(response.feed.data[i]);
-                                    }
+                            //console.log(promotable_posts);
+                            for(var i = 0; i < response.feed.data.length; i++) {
+                                if(response.feed.data[i].status_type == "wall_post") {
+                                  response.feed.data[i]["is_published"] = true;
+                                  $scope.pageData.promotable_posts.data.push(response.feed.data[i]);
+                                  console.log($scope.pageData.promotable_posts.data);
                                 }
+                            }
 
-                                function compare(a,b) {
-                                  if(a.created_time <= b.created_time) {
-                                    return 1;
-                                  }
-                                  else{
-                                    return -1;
-                                  }
-                                  return 0;
-                                }
-
-                                $scope.pageData.promotable_posts.data.sort(compare);
-
-                                console.log($scope.pageData.promotable_posts.data);
-                              },
-                              function(err) {
-                                    console.log("err");
-                              });
-
-                            
-
-                            var count = 0;
-                            for (var index = 0; index < promotable_posts.length; ++index) {
-                              var promotable_post = promotable_posts[index];
-                              //var impression;
-                              //promotable_post.impression = "";
-                              //console.log($scope.pageData.promotable_posts.data);
-                              var insightCache = cache.get("/"+promotable_post.id+"/insights/post_impressions");
-                              if(insightCache !== undefined) {
-                                   $scope.pageData.promotable_posts.data[count]["impression"] = insightCache;
-                                   continue;
-                                  //$scope.pageData.promotable_posts.data[index] = promotable_post.impression;
-                                  //return;
+                            function compare(a,b) {
+                              if(a.created_time <= b.created_time) {
+                                return 1;
                               }
+                              else{
+                                return -1;
+                              }
+                              return 0;
+                            }
+
+                            $scope.pageData.promotable_posts.data.sort(compare);
+                            console.log($scope.pageData.promotable_posts.data);        
+
+                            for (var index = 0; index < promotable_posts.length; ++index) {
                               
+                              var promotable_post = promotable_posts[index];
+                              console.log($scope.pageData.promotable_posts.data);
+                              
+                              /*
+                              var insightCache = cache.get("/"+promotable_post.id+"/insights/post_impressions");
+                              
+                              
+                              if(insightCache !== undefined) {
+                                   
+                                   for(var i = 0; i < $scope.pageData.promotable_posts.data.length; ++i) {
+                                        if($scope.pageData.promotable_posts.data[i].id == promotable_post.id) {
+                                            $scope.pageData.promotable_posts.data[i]["impression"] = insightCache;
+                                        }
+                    
+                                    }
+                                    continue;
+                              }
+                              */
                               $facebook.api("/"+promotable_post.id+"/insights/post_impressions").then(
                               
                                 function(response) {
-                                    loadInsight(response.data[0]);
+                                    var id = response.data[0].id.match(/[0-9]+_[0-9]+/);
+
+                                    for(var i = 0; i < $scope.pageData.promotable_posts.data.length; ++i) {
+                                        if($scope.pageData.promotable_posts.data[i].id == id) {
+                                            $scope.pageData.promotable_posts.data[i]["impression"] = response.data[0];
+                                        }
+                                    }
                                     cache.put("/"+promotable_post.id+"/insights/post_impressions", response.data[0]);
                                 },
                                 function(err) {
                                     console.log("err");
-                                });
-                                
-                                function loadInsight(aimpression) {
-                                  //console.log(count);
-                                  //promotable_post.impression = "";
-                                  //promotable_post.impression = aimpression
-                                  $scope.pageData.promotable_posts.data[count]["impression"] = aimpression;
-                                  //console.log($scope.pageData.promotable_posts.data[count]);
-                                  count++;
-                                }
+                              });
                             }
-                        },
-                        function(err) {
-                            console.log("err");
-                        });
+                          },
+                          function(err) {
+                              console.log("err");
+                          });
                       
                       $scope.post = function() {
                         $facebook.api("/"+$scope.pageId+"/feed", 'post', 
