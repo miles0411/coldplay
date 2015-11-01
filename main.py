6ba9d1427f8e28dd1adc7165d746eb0f38e96b7e
Preston Lin <prestonlin@fb.com>
2015-11-01 13:02:25 -0800

    flask fix

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ app.jinja_loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(os.path.
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
     return render_template('index.html')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect("/#/access/404")
+
+if __name__ == '__main__':
+    app.run()
