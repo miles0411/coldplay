53b1702b999b3dc817f6d72ea02febd631c19945
System Administrator <root@Prestons-iMac.local>
2015-11-01 19:11:33 -0800

    changing

--- static/js/config.router.js
+++ static/js/config.router.js
@@ -43,29 +43,104 @@ angular.module('app')
                         url: '/app',
                         templateUrl: '/static/templates/app.html'
                     })
-                    .state('app.dashboard', {
-                        url: '/dashboard/:pageId',
-                        templateUrl: '/static/templates/app_dashboard_v1.html',
-                        resolve: {
-                            deps: ['$ocLazyLoad',
-                                function($ocLazyLoad) {
-                                    return $ocLazyLoad.load(['static/js/controllers/chart.js']);
-                                }
-                            ]
-                        }
+                    .state('app.dashboard-v1', {
+                      url: '/dashboard-v1',
+                      templateUrl: '/static/templates/app_dashboard_v1.html',
+                      resolve: {
+                        deps: ['ocLazyLoad',
+                          function( $ocLazyLoad ){
+                            return $ocLazyLoad.load(['js/controllers/chart.js']);
+                        }]
+                      }
+                    })
+                    .state('app.dashboard-v2', {
+                      url: '/dashboard-v2',
+                      templateUrl: '/static/templates/app_dashboard_v2.html',
+                      resolve: {
+                        deps: ['ocLazyLoad',
+                          function( $ocLazyLoad ){
+                            return $ocLazyLoad.load(['js/controllers/chart.js']);
+                        }]
+                      }
                     })
                     .state('app.chart', {
                         url: '/chart',
                         templateUrl: '/static/templates/ui_chart.html',
                         resolve: {
-                            deps: ['$ocLazyLoad',
+                            deps: ['ocLazyLoad',
                                 function($ocLazyLoad) {
                                     return $ocLazyLoad.load('static/js/controllers/chart.js');
                                 }
                             ]
                         }
                     })
-                    // others
+              // table
+                    .state('app.table', {
+                      url: '/table',
+                      template: '<div ui-view></div>'
+                    })
+                    .state('app.table.static', {
+                      url: '/static',
+                      templateUrl: '/static/templates/table_static.html'
+                    })
+                    .state('app.table.datatable', {
+                      url: '/datatable',
+                      templateUrl: '/static/templates/table_datatable.html'
+                    })
+                    .state('app.table.footable', {
+                      url: '/footable',
+                      templateUrl: '/static/templates/table_footable.html'
+                    })
+                    .state('app.table.grid', {
+                      url: '/grid',
+                      templateUrl: '/static/templates/table_grid.html',
+                      resolve: {
+                          deps: ['ocLazyLoad',
+                            function( $ocLazyLoad ){
+                              return $ocLazyLoad.load('ngGrid').then(
+                                  function(){
+                                      return $ocLazyLoad.load('js/controllers/grid.js');
+                                  }
+                              );
+                          }]
+                      }
+                    })
+                    .state('app.form', {
+                      url: '/form',
+                      template: '<div ui-view class="fade-in"></div>',
+                      resolve: {
+                          deps: ['uiLoad',
+                            function( uiLoad){
+                              return uiLoad.load('js/controllers/form.js');
+                          }]
+                      }
+                    })
+                    .state('app.form.elements', {
+                      url: '/elements',
+                      templateUrl: '/static/templates/form_elements.html'
+                    })
+                    .state('app.form.validation', {
+                      url: '/validation',
+                      templateUrl: '/static/templates/form_validation.html'
+                    })
+                    .state('app.form.wizard', {
+                      url: '/wizard',
+                      templateUrl: '/static/templates/form_wizard.html'
+                    })
+                    .state('app.form.fileupload', {
+                      url: '/fileupload',
+                      templateUrl: '/static/templates/form_fileupload.html',
+                      resolve: {
+                          deps: ['ocLazyLoad',
+                            function($ocLazyLoad){
+                              return $ocLazyLoad.load('angularFileUpload').then(
+                                  function(){
+                                     return $ocLazyLoad.load('js/controllers/file-upload.js');
+                                  }
+                              );
+                          }]
+                      }
+                    })
                     .state('lockme', {
                         url: '/lockme',
                         templateUrl: '/static/templates/page_lockme.html'
@@ -78,9 +153,9 @@ angular.module('app')
                         url: '/signin',
                         templateUrl: '/static/templates/page_signin.html',
                         resolve: {
-                            deps: ['uiLoad',
-                                function(uiLoad) {
-                                    return uiLoad.load(['static/js/controllers/signin.js']);
+                            deps: ['ocLazyLoad',
+                                function($ocLazyLoad) {
+                                    return $ocLazyLoad.load(['static/js/controllers/signin.js']);
                                 }
                             ]
                         }
@@ -89,132 +164,90 @@ angular.module('app')
                         url: '/404',
                         templateUrl: '/static/templates/page_404.html'
                     })
-
-                // fullCalendar
-                .state('app.calendar', {
-                    url: '/calendar',
-                    templateUrl: '/static/templates/app_calendar.html',
-                    // use resolve to load other dependences
-                    resolve: {
-                        deps: ['$ocLazyLoad', 'uiLoad',
-                            function($ocLazyLoad, uiLoad) {
-                                return uiLoad.load(
-                                    ['/static/vendor/jquery/fullcalendar/fullcalendar.css',
-                                        '/static/vendor/jquery/fullcalendar/theme.css',
-                                        '/static/vendor/jquery/jquery-ui-1.10.3.custom.min.js',
-                                        '/static/vendor/libs/moment.min.js',
-                                        '/static/vendor/jquery/fullcalendar/fullcalendar.min.js',
-                                        'js/app/calendar/calendar.js'
-                                    ]
-                                ).then(
-                                    function() {
-                                        return $ocLazyLoad.load('ui.calendar');
-                                    }
-                                )
-                            }
-                        ]
-                    }
-                })
-
-                .state('layout', {
-                        abstract: true,
-                        url: '/layout',
-                        templateUrl: '/static/templates/layout.html'
+                    .state('app.form.imagecrop', {
+                      url: '/imagecrop',
+                      templateUrl: '/static/templates/form_imagecrop.html',
+                      resolve: {
+                          deps: ['ocLazyLoad',
+                            function($ocLazyLoad){
+                              return $ocLazyLoad.load('ngImgCrop').then(
+                                  function(){
+                                     return $ocLazyLoad.load('js/controllers/imgcrop.js');
+                                  }
+                              );
+                          }]
+                      }
                     })
-                    .state('layout.fullwidth', {
-                        url: '/fullwidth',
-                        views: {
-                            '': {
-                                templateUrl: '/static/templates/layout_fullwidth.html'
-                            },
-                            'footer': {
-                                templateUrl: '/static/templates/layout_footer_fullwidth.html'
-                            }
-                        },
+                    .state('app.form.select', {
+                      url: '/select',
+                      templateUrl: '/static/templates/form_select.html',
+                      controller: 'SelectCtrl',
+                      resolve: {
+                          deps: ['ocLazyLoad',
+                            function( $ocLazyLoad ){
+                              return $ocLazyLoad.load('ui.select').then(
+                                  function(){
+                                      return $ocLazyLoad.load('js/controllers/select.js');
+                                  }
+                              );
+                          }]
+                      }
+                    })
+                    .state('app.form.slider', {
+                      url: '/slider',
+                      templateUrl: '/static/templates/form_slider.html',
+                      controller: 'SliderCtrl',
+                      resolve: {
+                          deps: ['ocLazyLoad',
+                            function( $ocLazyLoad ){
+                              return $ocLazyLoad.load('vr.directives.slider').then(
+                                  function(){
+                                      return $ocLazyLoad.load('js/controllers/slider.js');
+                                  }
+                              );
+                          }]
+                      }
+                    })
+                    .state('app.form.editor', {
+                      url: '/editor',
+                      templateUrl: '/static/templates/form_editor.html',
+                      controller: 'EditorCtrl',
+                      resolve: {
+                          deps: ['ocLazyLoad',
+                            function( $ocLazyLoad ){
+                              return $ocLazyLoad.load('textAngular').then(
+                                  function(){
+                                      return $ocLazyLoad.load('js/controllers/editor.js');
+                                  }
+                              );
+                          }]
+                      }
+                    })
+                    .state('app.calendar', {
+                        url: '/calendar',
+                        templateUrl: '/static/templates/app_calendar.html',
+                        // use resolve to load other dependences
                         resolve: {
-                            deps: ['uiLoad',
-                                function(uiLoad) {
-                                    return uiLoad.load(['static/js/controllers/vectormap.js']);
-                                }
-                            ]
-                        }
-                    })
-                    .state('layout.mobile', {
-                        url: '/mobile',
-                        views: {
-                            '': {
-                                templateUrl: '/static/templates/layout_mobile.html'
-                            },
-                            'footer': {
-                                templateUrl: '/static/templates/layout_footer_mobile.html'
-                            }
-                        }
-                    })
-                    .state('layout.app', {
-                        url: '/app',
-                        views: {
-                            '': {
-                                templateUrl: '/static/templates/layout_app.html'
-                            },
-                            'footer': {
-                                templateUrl: '/static/templates/layout_footer_fullwidth.html'
-                            }
-                        },
-                        resolve: {
-                            deps: ['uiLoad',
-                                function(uiLoad) {
-                                    return uiLoad.load(['static/js/controllers/tab.js']);
-                                }
-                            ]
-                        }
-                    })
-                    .state('apps', {
-                        abstract: true,
-                        url: '/apps',
-                        templateUrl: '/static/templates/layout.html'
-                    })
-                    .state('apps.note', {
-                        url: '/note',
-                        templateUrl: '/static/templates/apps_note.html',
-                        resolve: {
-                            deps: ['uiLoad',
-                                function(uiLoad) {
-                                    return uiLoad.load(['js/app/note/note.js',
-                                        '/static/vendor/libs/moment.min.js'
-                                    ]);
-                                }
-                            ]
-                        }
-                    })
-                    .state('apps.contact', {
-                        url: '/contact',
-                        templateUrl: '/static/templates/apps_contact.html',
-                        resolve: {
-                            deps: ['uiLoad',
-                                function(uiLoad) {
-                                    return uiLoad.load(['js/app/contact/contact.js']);
-                                }
-                            ]
-                        }
-                    })
-                    .state('app.weather', {
-                        url: '/weather',
-                        templateUrl: '/static/templates/apps_weather.html',
-                        resolve: {
-                            deps: ['ocLazyLoad',
-                                function($ocLazyLoad) {
-                                    return $ocLazyLoad.load({
-                                        name: 'angular-skycons',
-                                        files: ['js/app/weather/skycons.js',
+                            deps: ['$ocLazyLoad', 'uiLoad',
+                                function($ocLazyLoad, $uiLoad) {
+                                    return $ocLazyLoad.load(
+                                        ['/static/vendor/jquery/fullcalendar/fullcalendar.css',
+                                            '/static/vendor/jquery/fullcalendar/theme.css',
+                                            '/static/vendor/jquery/jquery-ui-1.10.3.custom.min.js',
                                             '/static/vendor/libs/moment.min.js',
-                                            'js/app/weather/angular-skycons.js',
-                                            'js/app/weather/ctrl.js'
+                                            '/static/vendor/jquery/fullcalendar/fullcalendar.min.js',
+                                            'js/app/calendar/calendar.js'
                                         ]
-                                    });
+                                    ).then(
+                                        function() {
+                                            return $ocLazyLoad.load('ui.calendar');
+                                        }
+                                    )
                                 }
                             ]
                         }
                     })
+
             }
         ]
     );
