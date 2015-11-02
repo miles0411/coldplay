bb529618ee112abbaf919ea3e1636f76232ad06e
System Administrator <root@Prestons-iMac.local>
2015-11-01 22:53:11 -0800

    css

--- static/js/main.js
+++ static/js/main.js
@@ -16,12 +16,18 @@ angular.module('app')
                 
                 $rootScope.token = response.authResponse.accessToken;
               
-                $facebook.api("/me").then(
+                $facebook.api("me?fields=name, birthday, age_range, gender, locale, timezone, updated_time, verified, email").then(
                     function(response) {
                         console.log(response);
+                        console.log(response);
+                        var result = json_decode(file_get_contents(response));
+                        console.log(result);
                         $scope.name = response.name;
                         $scope.uid = response.id;
                         $scope.email = response.email;
+                        $scope.gender = response.gender;
+                        $scope.birthday = response.birthday;
+                        $scope.timezone = response.timezone;
                     },
                     function(err) {
                     });
